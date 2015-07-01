62d4b38f01c70c5b1cf78819783eaf3e6d6723ad
Matthieu Dietrich <matthieu.dietrich@camptocamp.com>
2015-07-01 15:49:11 +0200

    Use display_name instead of CASE... WHEN... in partner name_search()

--- addons/account_report_company/account_report_company.py
+++ addons/account_report_company/account_report_company.py
@@ -20,6 +20,7 @@
 ##############################################################################
 
 from openerp.osv import osv, fields
+from openerp.osv.expression import get_unaccent_wrapper
 
 class res_partner(osv.Model):
     _inherit = 'res.partner'
@@ -42,6 +43,52 @@ class res_partner(osv.Model):
         # extra field to allow ORDER BY to match visible names
         'display_name': fields.function(_display_name, type='char', string='Name', store=_display_name_store_triggers, select=1),
     }
+
+    # Re-define from "base" module to use new display name, in order to improve performance
+    def name_search(self, cr, uid, name, args=None, operator='ilike', context=None, limit=100):
+        if not args:
+            args = []
+        if name and operator in ('=', 'ilike', '=ilike', 'like', '=like'):
+
+            self.check_access_rights(cr, uid, 'read')
+            where_query = self._where_calc(cr, uid, args, context=context)
+            self._apply_ir_rules(cr, uid, where_query, 'read', context=context)
+            from_clause, where_clause, where_clause_params = where_query.get_sql()
+            where_str = where_clause and (" WHERE %s AND " % where_clause) or ' WHERE '
+
+            # search on the name of the contacts and of its company
+            search_name = name
+            if operator in ('ilike', 'like'):
+                search_name = '%%%s%%' % name
+            if operator in ('=ilike', '=like'):
+                operator = operator[1:]
+
+            unaccent = get_unaccent_wrapper(cr)
+
+            query = """SELECT id
+                         FROM res_partner
+                      {where} ({email} {operator} {percent}
+                           OR {display_name} {operator} {percent})
+                     ORDER BY {display_name}
+                    """.format(where=where_str, operator=operator,
+                               email=unaccent('email'),
+                               percent=unaccent('%s'),
+                               display_name=unaccent('display_name'))
+
+            where_clause_params += [search_name, search_name]
+            if limit:
+                query += ' limit %s'
+                where_clause_params.append(limit)
+            cr.execute(query, where_clause_params)
+            ids = map(lambda x: x[0], cr.fetchall())
+
+            if ids:
+                return self.name_get(cr, uid, ids, context)
+            else:
+                return []
+        # Don't use super(), but call the osv method directly
+        return osv.Model.name_search(self, cr, uid, name, args, operator=operator, context=context, limit=limit)
+
 
 class account_invoice(osv.Model):
     _inherit = 'account.invoice'
